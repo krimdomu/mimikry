c69a93a376e0c3d75456236f66c7e0c48c35fba3
Jan <krimdomu@googlemail.com>
2012-05-20 00:54:06 +0200

    added all the options needed for booting an os

--- apps/opendcm/models/support/boot.py
+++ apps/opendcm/models/support/boot.py
@@ -3,6 +3,10 @@ from django.conf import settings
                 
 class NextBoot(models.Model):
    name = models.CharField(max_length=100)
+   kernel = models.CharField(max_length=200)
+   template = models.TextField(blank=True)
+   initrd = models.CharField(max_length=200, blank=True)
+   cmdline = models.CharField(max_length=8096, blank=True)
    descr = models.CharField(max_length=4096, blank=True)
    
    def __unicode__(self):
